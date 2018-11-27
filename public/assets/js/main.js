31b41345c92d7c49844146f3f6b4b460575f3d98
 Rustem Mustazhap <russt@MacBook-Pro-Mustazhap.local>
2018-11-27 20:21:21 +0600

    adaptive for admin

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,48 +1,19 @@
  $(document).ready(function(){
-  $(".header__menu").on("click", function (event) {
-    if($(".block1").css("left") === "-405px"){
-      $(".block1").css({"top": "0", "left": "-0"});
-      $("body").css("overflow", "auto"); 
-      $(".block1").removeClass("menu_active");
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
+  $(".jobs__menu").click(function(){
+    if($(this).hasClass("animated")){
+      $(".sidebar").css("left", "0");
+        if (w > 600){
+        $(this).css("left","360px").removeClass("animated").find("i").css("transform", "rotate(180deg)");
+        }else{
+          $(this).css({"left":"90%"}).removeClass("animated").find("i").css("transform", "rotate(180deg)");
     }
-    else{
-      $("body").css("overflow", "hidden");      
-      $(".block1").css({"top": "100px", "left": "-405px"});
-      $(".block1").addClass("menu_active");
-    }
-  });
-
-  $(".menu__close").on("click", function (event) {
-      $("body").css("overflow", "auto");      
-      $(".block1").css({"top": "0", "left": "-0"});
-      $(".block1").removeClass("menu_active");
-  });
-
-  $(".block1").on("click", function (event) {
-    if($(".block1").css("left") === "-405px"){
-      $("body").css("overflow", "auto");      
-      $(".block1").css({"top": "0", "left": "-0"});
-      $(".block1").removeClass("menu_active");
-  }
-});
-
-
-  
-  $(".mob-menu").on("click", function (event) {
-    if($(".menu").css("right") === "-1000px"){
-      $(".menu").css({"right": "-0"});
-      $("body").css("overflow", "hidden");
-      $(".icon-menu").css("display", "none");
-      $(".icon-close").css("display", "block");
-
-
-    }
-    else{
-      $(".menu").css({"right": "-1000px"});
-      $("body").css("overflow", "auto");
-      $(".icon-close").css("display", "none");
-      $(".icon-menu").css("display", "block");
+    }else{
+      // $("body").css("overflow-x", "visible");
+      $(".sidebar").css("left", "-100%");
+      // $(".jobs").css({"position": "relative", "left": "0px"});
+      $(this).css("left","15px").addClass("animated").find("i").css("transform", "rotate(0)");  
     }
   });
 });

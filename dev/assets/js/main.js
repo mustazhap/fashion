92ecf9134a73a648de2f7c29e6b45a8b59ba3e8d
 Rustem Mustazhap <russt@MacBook-Pro-Mustazhap.local>
2018-11-28 20:17:47 +0600

    admin page

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,6 +1,7 @@
  $(document).ready(function(){
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
+  // Menu
   $(".jobs__menu").click(function(){
     if($(this).hasClass("animated")){
       $(".sidebar").css("left", "0");
@@ -16,4 +17,57 @@
       $(this).css("left","15px").addClass("animated").find("i").css("transform", "rotate(0)");  
     }
   });
+// 
+
+
+// Select menu
+
+$('select').each(function(){
+  var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+  $this.addClass('select-hidden'); 
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select-styled"></div>');
+
+  var $styledSelect = $this.next('div.select-styled');
+  $styledSelect.text($this.children('option').eq(0).text());
+
+  var $list = $('<ul />', {
+      'class': 'select-options'
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < numberOfOptions; i++) {
+      $('<li />', {
+          text: $this.children('option').eq(i).text(),
+          rel: $this.children('option').eq(i).val()
+      }).appendTo($list);
+  }
+
+  var $listItems = $list.children('li');
+
+  $styledSelect.click(function(e) {
+      e.stopPropagation();
+      $('div.select-styled.active').not(this).each(function(){
+          $(this).removeClass('active').next('ul.select-options').hide();
+      });
+      $(this).toggleClass('active').next('ul.select-options').toggle();
+  });
+
+  $listItems.click(function(e) {
+      e.stopPropagation();
+      $styledSelect.text($(this).text()).removeClass('active');
+      $this.val($(this).attr('rel'));
+      $list.hide();
+      //console.log($this.val());
+  });
+
+  $(document).click(function() {
+      $styledSelect.removeClass('active');
+      $list.hide();
+  });
+
+});
+
+// 
+
 });

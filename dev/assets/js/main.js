1601fdcbf5871d5ed6e92250216db2cd00b8d908
 Rustem Mustazhap <russt@MacBook-Pro-Mustazhap.local>
2018-11-29 18:17:56 +0600

    added apply modal for job

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -76,11 +76,61 @@ $('select').each(function(){
 // Textarea
 $(".textarea").click(function(event){
     if($(".contact__mail").css("right") == "10px"){
-        event.stopPropagation();
         $(".contact__mail").css({"opacity":"0", "visibility":"hidden"});
     }
     $(document).click(function() {
         $(".contact__mail").css({"opacity":"1", "visibility":"visible"});
     });
 })
+
+// 
+
+// File Upload
+$( '.file' ).each( function()
+	{
+		var $input	 = $( this ),
+			$label	 = $input.next( 'label' ),
+			labelVal = $label.html();
+
+		$input.on( 'change', function( e )
+		{
+			var fileName = '';
+
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else if( e.target.value )
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				$label.find( 'span' ).html( fileName );
+			else
+				$label.html( labelVal );
+		});
+
+		// Firefox bug fix
+		$input
+		.on( 'focus', function(){ $input.addClass( 'has-focus' ); })
+		.on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
+    });
+// 
+
+// Popup - Apply for Job
+
+    $(".apply-button").click(function(){
+        $(".apply").show().css("display", "flex");
+        $("body").css("overflow", "hidden");
+    });
+    $(".apply__wrapper").click(function(event){
+        event.stopPropagation();        
+    });
+    $(".fa-times").click(function(){
+        $(".apply").hide();
+        $("body").css("overflow", "auto");
+        
+    });
+    $(".apply").click(function(){
+        $(".apply").hide();
+        $("body").css("overflow", "auto");
+
+    });
 });
